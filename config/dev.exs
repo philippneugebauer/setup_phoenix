8a7641a11e560c16103c4c74ccd5b308005e76bb
Philipp Neugebauer <ppn@philippneugebauer.net>
2018-04-10 16:45:20 +0200

    set postgres connection details

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :setup_phoenix, SetupPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "ppn",
+  password: "",
   database: "setup_phoenix_dev",
   hostname: "localhost",
   pool_size: 10
