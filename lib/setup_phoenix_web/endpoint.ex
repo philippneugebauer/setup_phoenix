4c284f8bf6b8a278995a6210372234ff99fbc2d1
Philipp Neugebauer <ppn@philippneugebauer.net>
2018-05-23 14:46:00 +0200

    not used user socket removed

--- lib/setup_phoenix_web/endpoint.ex
+++ lib/setup_phoenix_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule SetupPhoenixWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :setup_phoenix
 
-  socket "/socket", SetupPhoenixWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
